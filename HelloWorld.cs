9083fa1827163f1004ec18defd4d65ca913f98b5
Atrusan <rolgalthor@gmail.com>
2025-04-02 18:50:55 -0500

    Seriously need to add a comment to this

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+// This is a simple C# program that prints "Hello, World!" to the console.
+// It demonstrates the basic structure of a C# program, including the use of namespaces, classes, and methods.
