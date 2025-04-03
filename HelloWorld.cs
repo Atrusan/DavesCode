5cfe60a2d410112be827b0d89e13ad95f40d368d
Atrusan <rolgalthor@gmail.com>
2025-04-02 19:22:29 -0500

    setting debug config

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,5 +6,3 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
-// This is a simple C# program that prints "Hello, World!" to the console.
-// It demonstrates the basic structure of a C# program, including the use of namespaces, classes, and methods.
